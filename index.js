df0fce8c1c1a3bb004f95e16cee2660626127193
AJ <aj@smartnvm.com>
2021-09-24 01:54:27 +0000

    spotter II fetch coords by ip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 
 
@@ -11,6 +11,20 @@ fetchMyIP((error, ip) => {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+
+
 });
+ 
+
+fetchCoordsByIP('135.23.201.52', (error, lat, lon) => {
+ 
+
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('Lat:', lat, 'Long:' , lon )
+});
+
--- iss.js
+++ iss.js
@@ -19,11 +19,11 @@ const fetchMyIP = function (callback) {
 
     if (error) return callback(`\n ERROR: ${error.message}`, null)
 
-    if (response.statusCode !== 200 ){ 
+    if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null)
-      
+
     }
-    
+
     //you can fetch IP also by response.body
     const ip = JSON.parse(body).ip
     callback(null, ip) // Print the HTML for the Google homepage.
@@ -34,8 +34,31 @@ const fetchMyIP = function (callback) {
 
 
 
+const fetchCoordsByIP = function (ip, callback) {
+  // use request to fetch IP address from JSON API
+  
+
+  request('https://freegeoip.app/json/' + ip, (error, response, body) => {
+
+    if (error) return callback(`\n ERROR: ${error.message}`, null)
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null)
+
+    }
+
+    //you can fetch IP also by response.body
+    //  const ip = JSON.parse(body).ip
+    const lat = JSON.parse(body).latitude
+    const long = JSON.parse(body).longitude
+
+    callback(null, lat, long)
+
+  })
+
+}
 
 
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
